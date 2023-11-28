788ad7628bb82853d048e11ffac9a2f134b6c969
Derjai <ojaider@uninorte.edu.co>
2023-11-27 21:59:29 -0500

    Log creado
    Se implementó el historial de las transacciones

---
+++ proyecto_Software/Log_Transacciones/Controllers/LogController.cs
@@ -0,0 +1,133 @@
+﻿using Log_Transacciones.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using MongoDB.Driver;
+
+namespace Log_Transacciones.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class LogController : ControllerBase
+    {
+        private readonly IMongoCollection<Log> _logCollection;
+
+        public LogController()
+        {
+            var dbHost = Environment.GetEnvironmentVariable("DB_HOST");
+            var dbName = Environment.GetEnvironmentVariable("DB_NAME");
+            var connectionString = $"mongodb://{dbHost}:27017/{dbName}";
+            var mongoUrl = MongoUrl.Create(connectionString);
+            var mongoClient = new MongoClient(mongoUrl);
+            var dataBase = mongoClient.GetDatabase(mongoUrl.DatabaseName);
+            _logCollection = dataBase.GetCollection<Log>("log_transacciones");
+        }
+
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
+        [ProducesResponseType(StatusCodes.Status500InternalServerError)]
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<Log>>> GetLogs()
+        {
+            try 
+            { 
+                var logs = await _logCollection.Find(Builders<Log>.Filter.Empty).ToListAsync();
+                if (logs == null) return NotFound("No existen registros en la base de datos");
+                return Ok(logs); 
+            } 
+            catch(Exception ex) 
+            { 
+                Console.WriteLine(ex.Message);
+                return StatusCode(StatusCodes.Status500InternalServerError, ex.Message);
+            }
+            
+        }
+
+
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
+        [ProducesResponseType(StatusCodes.Status500InternalServerError)]
+        [HttpGet("/api/Log/doc/{num_doc}")]
+        public async Task<ActionResult<IEnumerable<Log>>> GetByDoc(string num_doc)
+        {
+            try
+            {
+                var filterDefinition = Builders<Log>.Filter.Eq(x => x.Num_Doc, num_doc);
+                var logs = await _logCollection.Find(filterDefinition).ToListAsync();
+                if (logs == null) return NotFound($"No hay registros de transacciones con el numero documento {num_doc}");
+                return Ok(logs);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+                return StatusCode(StatusCodes.Status500InternalServerError, ex.Message);
+            }
+
+        }
+
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
+        [ProducesResponseType(StatusCodes.Status500InternalServerError)]
+        [HttpGet("/api/Log/fecha/{fecha}")]
+        public async Task<ActionResult<IEnumerable<Log>>> GetByDate(DateTime fecha)
+        {
+            try
+            {
+                var filterDefinition = Builders<Log>.Filter.Eq(x => x.Fecha, fecha);
+                var logs = await _logCollection.Find(filterDefinition).ToListAsync();
+                if (logs == null) return NotFound($"No hay registros de transacciones en la fecha: {fecha}");
+                return Ok(logs);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+                return StatusCode(StatusCodes.Status500InternalServerError, ex.Message);
+            }
+            
+        }
+
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
+        [ProducesResponseType(StatusCodes.Status500InternalServerError)]
+        [HttpGet("/api/Log/fecha/{fecha}")]
+        public async Task<ActionResult<IEnumerable<Log>>> GetByType(string tipo)
+        {
+            try
+            {
+                var filterDefinition = Builders<Log>.Filter.Eq(x => x.Tipo_Transaccion, tipo);
+                var logs = await _logCollection.Find(filterDefinition).ToListAsync();
+                if (logs == null) return NotFound($"No hay registros de transacciones de tipo {tipo}");
+                return Ok(logs);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+                return StatusCode(StatusCodes.Status500InternalServerError, ex.Message);
+            }
+
+        }
+
+        [HttpGet("{id}", Name = "GetLog")]
+        public async Task<ActionResult<Log>> GetLog(string id)
+        {
+            var filterDefinition = Builders<Log>.Filter.Eq(x => x.Id_Log, id);
+            return await _logCollection.Find(filterDefinition).FirstOrDefaultAsync();
+        }
+
+        [ProducesResponseType(StatusCodes.Status201Created)]
+        [ProducesResponseType(StatusCodes.Status500InternalServerError)]
+        [HttpPost]
+        public async Task<ActionResult<Log>> CreateLog([FromBody] Log log)
+        {
+            try
+            {
+                await _logCollection.InsertOneAsync(log);
+                return CreatedAtAction(nameof(GetLog), new { id = log.Id_Log }, log);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"Error al insertar en MongoDB: {ex}");
+                return StatusCode(500, "Error interno del servidor");
+            }
+        }
+    }
+}
---
+++ proyecto_Software/Log_Transacciones/Models/Log.cs
@@ -0,0 +1,23 @@
+﻿using MongoDB.Bson.Serialization.Attributes;
+using MongoDB.Bson;
+using MongoDB.Bson.Serialization.IdGenerators;
+
+namespace Log_Transacciones.Models
+{
+    [Serializable, BsonIgnoreExtraElements]
+    public class Log
+    {
+        [BsonId(IdGenerator = typeof(StringObjectIdGenerator))]
+        [BsonElement("_id"), BsonRepresentation(BsonType.ObjectId)]
+        public string Id_Log { get; set; } = null!;
+
+        [BsonElement("num_doc"), BsonRepresentation(BsonType.String)]
+        public string Num_Doc { get; set; } = null!;
+
+        [BsonElement("tipo_transaccion"), BsonRepresentation(BsonType.String)]
+        public string Tipo_Transaccion { get; set; } = null!;
+
+        [BsonElement("fecha"), BsonRepresentation(BsonType.DateTime)]
+        public DateTime Fecha { get; set; }
+    }
+}
